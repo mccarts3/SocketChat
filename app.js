5d1484ad3862327e411b64f3e29fc51eed857fcc
mccarts3 <mccarts3@miamioh.edu>
2017-02-13 20:54:17 -0500

    Added dynamic PORT instead of just local

--- app.js
+++ app.js
@@ -68,6 +68,4 @@ app.get('/', function (req, res) {
 app.use(express.static('public'));
 app.use('/bower_components', express.static('bower_components'));;
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+app.listen(process.env.PORT || 3000);
