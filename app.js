d02e74da833aef60d7ce051a65ad93b4dc5a880c
mccarts3 <mccarts3@miamioh.edu>
2017-08-12 15:00:02 -0400

    Removing Socket.io functionality for v2.0

--- app.js
+++ app.js
@@ -4,58 +4,58 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-
-var socket;
-
-var users = [];
-var numUsers = 0;
-
-function addUser(username) {
-	var userTaken = false;
-	for(var i = 0; i < users.length; i++) {
-		if(users[i].username == username) {
-			userTaken = true;
-			console.log("Username is already taken. Assigning anonymous username...");
-		}
-	}
-	if(username === "Anon") {	userTaken = true;	}
-			
-	if(!userTaken) {	
-		users.push({id: numUsers, username: username});
-		console.log(users[numUsers]);
-		numUsers++;	}
-	else {
-		users.push({id: numUsers, username: "Anon"+numUsers});		
-		console.log(users[numUsers]);		
-		numUsers++;
-	}
-}
-
-function deleteUser(username) {
-	for(var i = 0; i < users.length; i++) {
-		if(users[i].username == username) {
-			users.splice(i, 1);
-			numUsers--;
-		}
-	}
-}
-
-io.on('connection', function(socket){
-	io.emit('connect');
-	
-	socket.on('server_receive_username', function(username) {
-		addUser(username);
-		io.emit('user_connected', users[numUsers-1]);
-	}); 
-
-  socket.on('new_message', function(messageInfo){
-  		io.emit('add_message', {username: users[messageInfo.id].username, message: messageInfo.message});
-  });
-  
-	socket.on('disconnect', function() {
-		console.log('User disconnected.');
-	});
-});
+//
+//var socket;
+//
+//var users = [];
+//var numUsers = 0;
+//
+//function addUser(username) {
+//	var userTaken = false;
+//	for(var i = 0; i < users.length; i++) {
+//		if(users[i].username == username) {
+//			userTaken = true;
+//			console.log("Username is already taken. Assigning anonymous username...");
+//		}
+//	}
+//	if(username === "Anon") {	userTaken = true;	}
+//			
+//	if(!userTaken) {	
+//		users.push({id: numUsers, username: username});
+//		console.log(users[numUsers]);
+//		numUsers++;	}
+//	else {
+//		users.push({id: numUsers, username: "Anon"+numUsers});		
+//		console.log(users[numUsers]);		
+//		numUsers++;
+//	}
+//}
+//
+//function deleteUser(username) {
+//	for(var i = 0; i < users.length; i++) {
+//		if(users[i].username == username) {
+//			users.splice(i, 1);
+//			numUsers--;
+//		}
+//	}
+//}
+//
+//io.on('connection', function(socket){
+//	io.emit('connect');
+//	
+//	socket.on('server_receive_username', function(username) {
+//		addUser(username);
+//		io.emit('user_connected', users[numUsers-1]);
+//	}); 
+//
+//  socket.on('new_message', function(messageInfo){
+//  		io.emit('add_message', {username: users[messageInfo.id].username, message: messageInfo.message});
+//  });
+//  
+//	socket.on('disconnect', function() {
+//		console.log('User disconnected.');
+//	});
+//});
 
 //[DEFINE CONFIG VARS]
 app.get('/times', function(request, response) {
