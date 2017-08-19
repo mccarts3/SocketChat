0922f323607f3c7f71bb3f849e936d67b34953ab
mccarts3 <mccarts3@miamioh.edu>
2017-08-19 12:33:59 -0400

    Fixing body layout and updating page redirects

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+var json_request = require('request-json');
+var client = json_request.createClient('http://localhost:5000/#/');
 
 //[MAIN APP HTML ROUTE]
 app.get('/', function (req, res) {
 	var homePath = '/public/webpages/home.html';
 	var indexPath = '/index.html';
   res.sendFile(__dirname + indexPath);
+});
+
+app.get('/api/get-repos', function(request, response) {
+	client.get('https://api.github.com/users/mccarts3/repos', function(err, res, body) {
+		return response.json(body);
+	});
 });
 
 //[STATIC ROUTE DEFINITIONS]
--- public/indexAngular.js
+++ public/indexAngular.js
@@ -3,12 +3,27 @@
 	
 	app.config(function($routeProvider) {
 		$routeProvider
-		.when('/', {	templateUrl: '/webpages/home.html'	})	
-		.when('/home', {	templateUrl: '/webpages/home.html'	})
-		.when('/bio', {		templateUrl: '/webpages/about-me.html'	})
-		.when('/java', {	templateUrl: '/webpages/java.html'	})
-		.when('/engineering', {	templateUrl: '/webpages/engineering.html'	})
-		.when('/data-structures', {	templateUrl: '/webpages/data-structures.html'	})
-		.when('/error', {	templateUrl: '/webpages/status/error404.html'	});
+		.when('/', {	
+			templateUrl: '/webpages/home.html'
+		}).when('/home', {	
+			templateUrl: '/webpages/home.html'
+		}).when('/java', {
+			templateUrl: '/webpages/java.html'
+		}).when('/engineering', {
+			templateUrl: '/webpages/engineering.html'
+		}).when('/cpp',	{
+			templateUrl: '/webpages/data-structures.html'
+		}).otherwise({	templateUrl: '/webpages/status/error404.html'	});
 	});
+	
+	app.controller('repoCtrl', ['$scope', '$http', function($scope, $http) {
+		  $scope.myData = '';
+		  var updateData = function() {
+			  $http.get('/api/get-repos').then(function(response) {
+			  		$scope.myData = response.data;
+			  });
+		  };
+		  
+		  updateData();
+	}]);
 })();
