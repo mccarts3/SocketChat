071fd2c6ae4787883b00db17fd13fcd702277bb6
mccarts3 <mccarts3@miamioh.edu>
2017-08-19 12:42:24 -0400

    Adding more webpages and moving repo list to sectionNav div

--- public/indexAngular.js
+++ public/indexAngular.js
@@ -3,17 +3,14 @@
 	
 	app.config(function($routeProvider) {
 		$routeProvider
-		.when('/', {	
-			templateUrl: '/webpages/home.html'
-		}).when('/home', {	
-			templateUrl: '/webpages/home.html'
-		}).when('/java', {
-			templateUrl: '/webpages/java.html'
-		}).when('/engineering', {
-			templateUrl: '/webpages/engineering.html'
-		}).when('/cpp',	{
-			templateUrl: '/webpages/data-structures.html'
-		}).otherwise({	templateUrl: '/webpages/status/error404.html'	});
+		.when('/', {				templateUrl: '/webpages/home.html'	})
+		.when('/home', {			templateUrl: '/webpages/home.html'	})
+		.when('/java', {			templateUrl: '/webpages/java.html'	})
+		.when('/engineering', {		templateUrl: '/webpages/engineering.html'	})
+		.when('/cpp',	{			templateUrl: '/webpages/data-structures.html'	})
+		.when('/web',	{			templateUrl: '/webpages/web-development.html'	})
+		.when('/work-experience', {	templateUrl: '/webpages/work-experience.html'	})
+		.otherwise({				templateUrl: '/webpages/status/error404.html'	});
 	});
 	
 	app.controller('repoCtrl', ['$scope', '$http', function($scope, $http) {
